d893598597533727056022934174876bb8a49864
stakahashi <kyuuketukishouta@gmail.com>
2024-09-27 17:37:03 +0900

    feat: Postリクエストを実装

--- server.js
+++ server.js
@@ -8,52 +8,16 @@ const portNumber = 80;
 // appオブジェクトを作成する
 const app = express();
 
-// // インスタンスの作成
-// const client = new Client({
-//     user: "postgres",
-//     host: "localhost",
-//     database: "todolist",
-//     password: "tetraDev2024",
-//     port: "5432"
-// });
-
-/*
-JSON返信
-app.get("/", (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': '*' });
-    // res.status(200).send({ id: 1, message: "DBサーバーからのメッセージ" });
-    res.status(200).send(ViewTable());
-});
-
-// リクエストを待ち受ける
-app.listen(portNumber);
-
-console.log(`PortNumber is ${portNumber}`);
-*/
-
-// main();
-// async function main(){
-    // console.log(await ViewTable());
-    // // tasks = await ViewTable();
-
-    // // JSON返信
-    // app.get("/", async(req, res) => {
-    // res.set({ 'Access-Control-Allow-Origin': '*' });
-    // res.status(200).send(await ViewTable());
-    // });
-
-    // // リクエストを待ち受ける
-    // app.listen(portNumber);
-
-    // console.log(`PortNumber is ${portNumber}`);
-// }
-
-// console.log(await ViewTable());
-
-// JSON返信
+// Getリクエスト
 app.get("/", async(req, res) => {
-res.set({ 'Access-Control-Allow-Origin': '*' });
-res.status(200).send(await ViewTable());
+    res.set({ 'Access-Control-Allow-Origin': '*' });
+    res.status(200).send(await ViewTable());
+});
+
+// Postリクエスト
+app.post("/:task", async(req, res) => {
+    res.set({ 'Access-Control-Allow-Origin': '*' });
+    res.status(200).send(await RegisterTask(req.params.task));
 });
 
 // リクエストを待ち受ける
@@ -61,7 +25,7 @@ app.listen(portNumber);
 
 console.log(`PortNumber is ${portNumber}`);
 
-// 関数
+// テーブル表示
 async function ViewTable(){
     // インスタンスの作成
     const client = new Client({
@@ -76,22 +40,32 @@ async function ViewTable(){
 
     const query = "SELECT id, task, to_char(deadline,'YYYY年MM月DD日HH時MI分SS秒') AS deadline FROM tasks ORDER BY id";
 
-    /*
-    let result;
-    client.query(query)
-    .then((res) =>{
-        result = res.rows;
-        client.end();
-        return result;
-    })
-    .catch((err) => {
-        console.error(err.stack);
-        client.end();
-    });
-    */
-
     const res = await client.query(query);
     result = res.rows;
+    await client.end();
+    return result;
+}
+
+// レコード追加
+async function RegisterTask(task){
+    // インスタンスの作成
+    const client = new Client({
+        user: "postgres",
+        host: "localhost",
+        database: "todolist",
+        password: "tetraDev2024",
+        port: "5432"
+    });
+
+    client.connect();
+
+    const query = {
+        text: "INSERT INTO tasklist (task) VALUES ($1)",
+        values: [task],
+    };
+
+    await client.query(query);
+    result = "タスクが追加されました";
     await client.end();
     return result;
 }
